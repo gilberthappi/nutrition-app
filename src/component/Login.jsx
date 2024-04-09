72e3c705cf019e8b01f3478e299a4d51e448993a
gilberthappi <gdushimimana6@gmail.com>
2024-04-09 13:55:09 +0200

    Add componnt register and login

---
+++ src/component/Login.jsx
@@ -0,0 +1,25 @@
+import {Link} from 'react-router-dom';
+
+
+export default function Login(){
+
+    return(
+        <>
+        <section className="form-parent">
+          <form className="form">
+
+            <h1> Login To Fitness</h1>
+
+            <input className="inp" type="email" placeholder="Enter Email" name="email"/>
+
+            <input className="inp" type="password" placeholder="Enter Password" name="password"/>
+
+            <button className="btn">Login</button>
+
+            <p> Not Registered ? <Link to="/register">Register Now</Link></p>
+
+          </form>
+        </section>
+        </>
+    )
+}
---
+++ src/component/Register.jsx
@@ -0,0 +1,28 @@
+import {Link} from 'react-router-dom';
+
+export default function Register(){
+
+    return(
+        <>
+        <section className="form-parent">
+          <form className="form">
+
+            <h1> Start Your Fitness</h1>
+
+            <input className="inp" type="text" placeholder="Enter Name" name="name"/>
+
+            <input className="inp" type="email" placeholder="Enter Email" name="email"/>
+
+            <input className="inp" type="password" placeholder="Enter Password" name="password"/>
+
+            <input className="inp" type="number" placeholder="Enter Age" name="age"/>
+
+            <button className="btn">Join</button>
+
+            <p> Already Registered ? <Link to="/login">Login</Link></p>
+
+          </form>
+        </section>
+        </>
+    )
+}
