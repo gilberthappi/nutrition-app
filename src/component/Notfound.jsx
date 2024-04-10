d542db0dc3b5021f5de764796ba96103df77d21e
gilberthappi <gdushimimana6@gmail.com>
2024-04-10 20:46:11 +0200

    Add notfound

---
+++ src/component/Notfound.jsx
@@ -0,0 +1,10 @@
+export default function Notfound(){
+    return(
+     <section className="container nf">
+        <div className="not-found">
+            <h1>404 | Not Found</h1>
+            <p>Sorry, the page you are looking for might have been removed or is temporarily unavailable</p>
+        </div>
+        </section>
+    )
+}
