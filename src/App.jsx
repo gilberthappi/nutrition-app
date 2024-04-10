e3c1a0fcd9f5aad5ab120a56d172f6e4b9e3920c
gilberthappi <gdushimimana6@gmail.com>
2024-04-10 20:44:13 +0200

    Add new Route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './component/Register'
 import Login from './component/Login'
+import Notfound from './component/Notfound';
 
 
 function App() {
@@ -17,6 +18,9 @@ function App() {
         <Route path="/" element={<Login/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element= {<Register/>}/>
+        
+
+        <Route path="*" element={<Notfound/>}/>
 
         </Routes>
 
